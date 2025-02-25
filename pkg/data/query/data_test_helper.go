42e891211ce45e0c229882282f5cd1869920853e
Kasra Rasaee <krasaee@gmail.com>
2025-02-25 09:10:30 -0800

    fix test case invalid dependency reference

--- pkg/data/query/data_test_helper.go
+++ pkg/data/query/data_test_helper.go
@@ -2,13 +2,14 @@ package data
 
 import (
 	"fmt"
+	"github.com/quantumwake/alethic-ism-core-go/pkg/data"
 	"testing"
 )
 
-func TestNewDataAccess(t *testing.T) (*Access, error) {
+func TestNewDataAccess(t *testing.T) (*data.Access, error) {
 	//func TestNewDataAccess(t *testing.T) *data.Access {
 
-	da := NewDataAccess("host=localhost port=5432 user=postgres password=postgres1 dbname=postgres sslmode=disable")
+	da := data.NewDataAccess("host=localhost port=5432 user=postgres password=postgres1 dbname=postgres sslmode=disable")
 	if da == nil {
 		if t != nil {
 			t.Errorf("unable to connect to database")
